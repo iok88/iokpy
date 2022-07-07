2fbd1749f660d81ddfd7c238e219fa4906a1f4c1
wrok <6561657@gmail.com>
2022-07-07 16:20:19 +0300

    070722

--- xoxo.py
+++ xoxo.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-#r = requests.get("https://stopgame.ru/review/new/izumitelno/p1")
-r = requests.get("https://mysku.club/")
-html = BS(r.content, 'html.parser')
+page = 1
 
-#for el in html.select(".items > .article-summary"):
-#    title = el.select('.caption > a')
-#    print( title[0].text)
+while True:
+    # r = requests.get("https://stopgame.ru/review/new/izumitelno/p1")
+    r = requests.get("https://mysku.club/index/page" + str(page))
+    html = BS(r.content, 'html.parser')
+    items = html.select("#content > .topic")
 
-for el in html.select("#content > .topic"):
-    title = el.select('.topic-title > a')
-    print( title[0].text)
+    if(len(items)):
+        for el in items:
+            title = el.select('.topic-title > a')
+            print(title[0].text)
+        page += 1
+        print('page page  ' + str(page))
+    else:
+        break
+
+
+    # for el in html.select(".items > .article-summary"):
+    #    title = el.select('.caption > a')
+    #    print( title[0].text)
