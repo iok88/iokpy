b9b6ab2cc895bf11f65c1079044a67b3cba77e90
iok88 <656167@gmail.com>
2022-07-10 23:17:21 +0300

    Notebook 10072022

--- selenium_insta_chrom.py
+++ selenium_insta_chrom.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from fake_useragent import UserAgent
 from selenium.webdriver.chrome.service import Service
 #from selenium.webdriver.firefox.service import Service
+import time
 import requests
 from bs4 import BeautifulSoup as BS
+
 
 useragent = UserAgent()
 options = webdriver.ChromeOptions()
 options.add_argument(f"user-agent={useragent.opera}")
-options.add_argument(f"--proxy-server=45.152.188.246:3128")
+
 
 s=Service('e:/python/chromedriver_win32/chromedriver.exe')
 #s=Service('e:/python/geckodriver/geckodriver.exe')
@@ -19,13 +22,26 @@ s=Service('e:/python/chromedriver_win32/chromedriver.exe')
 driver = webdriver.Chrome(service=s, options=options)
 #url = 'https://www.kufar.by/l/velosipedy'
 #url='https://www.whatismybrowser.com/detect/what-is-my-user-agent/'
-url = 'https://www.2ip.ru'
+url = 'https://www.instagram.com/'
 
 try:
     driver.get(url=url)
-    #driver.get_cookie()
+    time.sleep(10)
 
-    timer.sleep(10)
+    email_input = driver.find_element("name",'username')
+    email_input.clear()
+    email_input.send_keys("iok88")
+    time.sleep(10)
+
+    password_input = driver.find_element("name",'password')
+    password_input.clear()
+    password_input.send_keys("1988Iokkoi")
+    time.sleep(10)
+
+    password_input.send_keys(Keys.ENTER)
+    # login_button = driver.find_element().click()
+    time.sleep(10)
+
 except Exception as ex:
     print(ex)
 finally:
